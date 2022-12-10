3589f943c6a2018c832a56c16c1ee1c32b93f3fc
shubham6705 <shubhamsharma6705@gmail.com>
2022-12-10 23:40:35 +0530

    some changes

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('projectDetail/create/', views.ProjectCreationApiView.as_view(), name="ProjectCreationApiUrl"),
     path('login/', views.MyObtainTokenPairView.as_view(), name="MyObtainTokenPairUrl"),
     path('login/refresh/',TokenRefreshView.as_view(), name="TokenRefreshUrl"),
-    path('project-list/', views.productListApiView, name="projectlisturl")
+    path('projects/', views.productListApiView, name="projectlisturl")
 ]
--- sampledev/urls.py
+++ sampledev/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('project/', include('projectApp.urls', namespace="projectApp"))
+    path('', include('projectApp.urls', namespace="projectApp"))
 ]
